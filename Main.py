8f92464fc583a91da8a41a4472575f38e6d82d0b
johnny-appleseed-69420 <mwmkurtis@gmail.com>
2024-12-13 00:27:18 -0500

    some stuff, almost got foldersort working, singlesort is good rn

--- Main.py
+++ Main.py
@@ -1,10 +1,28 @@
 #My script imports
-from Pixelsorting import pixelsorter
+from Pixelsorting import pixelsorter, foldersorter
 from UserInput import userInput
+from dotenv import load_dotenv
+import os
 
+load_dotenv()#testing
+directory = os.getenv("\n\n\nfolder_input_path")
+
+# Print to check if it's correct
+print(f"Directory path: {directory}")
 #greeting
-print("Hello and welcome to the Thrush Band Incorporated Image Pulverizer.  Please insert tip.\n\n")
-
-#runs userinput script, then pixelsorter script
-input_path, output_path = userInput()
-pixelsorter(input_path, output_path)
+print("Hello and welcome to the Thrush Band Incorporated Image Pulverizer.  Please insert tip.\n")
+SingleOrMulti = input("type f for a whoole folder(SortAll), else you get one pic.\n")
+if SingleOrMulti == "f":
+    #directory = os.getenv("folder_input_path")
+    directory = "C:\\Users\\mwk\\Desktop\\thrushArt\\PICTURESToSORT\\Unsorted\\SortAll\\"
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        if os.path.isfile(f):
+            input_path = f
+            output_path = os.getenv("output_path")
+            print(f)
+            foldersorter(f, output_path)
+else:
+    #runs userinput script, then pixelsorter script
+    input_path, output_path = userInput()
+    pixelsorter(input_path, output_path)
